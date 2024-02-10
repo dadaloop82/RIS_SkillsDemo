749b6518a5e2434e87076858abba9e67edc50574
dadaloop82 <dadaloop82@gmail.com>
2024-02-10 22:06:16 +0100

    configure pom and WebMvcConfig for serving react files

--- src/main/java/com/dadaloop/RISSkillDemo/config/WebMvcConfig.java
+++ src/main/java/com/dadaloop/RISSkillDemo/config/WebMvcConfig.java
@@ -3,32 +3,15 @@ package com.dadaloop.RISSkillDemo.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.web.servlet.resource.PathResourceResolver;
-
-import java.io.IOException;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/META-INF/resources/", "classpath:/resources/", "classpath:/static/",
-                        "classpath:/public/")
-                .resourceChain(true)
-                .addResolver(pathResourceResolver());
-    }
-
-    private PathResourceResolver pathResourceResolver() {
-        return new PathResourceResolver() {
-            @Override
-            protected Resource getResource(String resourcePath, Resource location) throws IOException {
-                Resource requestedResource = location.createRelative(resourcePath);
-                return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-                        : new ClassPathResource("/public/index.html");
-            }
-        };
+                .addResourceLocations("file:src/main/frontend/public/");
     }
 }
