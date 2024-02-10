5f278d99c6892f1cd0c36ed8d39583977d798be0
dadaloop82 <dadaloop82@gmail.com>
2024-02-10 22:31:26 +0100

    setup react env and hello world

---
+++ src/main/frontend/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: `${__dirname}/public`,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
