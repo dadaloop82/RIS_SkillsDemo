6f89ea814734c68f66ea141f74a670b73f30ca08
DanielS <Daniel>
2024-02-12 16:15:43 +0100

    update readme

--- src/main/java/com/dadaloop/RISSkillDemo/config/WebMvcConfig.java
+++ src/main/java/com/dadaloop/RISSkillDemo/config/WebMvcConfig.java
@@ -5,20 +5,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-/**
- * WebMvcConfig is a configuration class that implements WebMvcConfigurer interface
- * to provide custom configurations for Spring MVC. By annotating this class with
- * @Configuration, we declare it as a source of bean definitions. 
- */
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     /**
-     * Override the addResourceHandlers method to customize the serving of static resources.
-     * Here, we specify where Spring MVC should look for static resources such as CSS, JS,
-     * or image files. This setup is crucial when serving a Single Page Application (SPA)
-     * built with a framework like React alongside a Spring Boot application.
      *
      * @param registry ResourceHandlerRegistry to configure resource handlers for serving
      *                 static resources.
--- src/main/java/com/dadaloop/RISSkillDemo/repository/NoteRepository.java
+++ src/main/java/com/dadaloop/RISSkillDemo/repository/NoteRepository.java
@@ -4,20 +4,8 @@ import com.dadaloop.RISSkillDemo.model.NoteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * The NoteRepository interface extends the JpaRepository interface from Spring Data JPA.
- * This interface manages the database operations for the NoteEntity.
- * It provides CRUD (Create, Read, Update, Delete) operations and more on the NoteEntity.
- *
- * By extending JpaRepository, NoteRepository inherits several methods for working with NoteEntity persistence,
- * including methods for saving, deleting, and finding NoteEntity instances.
- *
- * @Repository annotation indicates that the class provides the mechanism for storage, retrieval,
- * update, delete and search operation on objects.
- */
+
 @Repository
 public interface NoteRepository extends JpaRepository<NoteEntity, Long> {
-    // Here you can add custom methods for interacting with the database, if needed.
-    // For example, if you need to find notes by their title, you could add:
-    // List<NoteEntity> findByTitle(String title);
+  
 }
